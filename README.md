46871ccdad79434bde3193c218ee7ea674bf4236
Li Jiale <ljl_security@163.com>
2016-06-24 15:50:31 +0800

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# demo-js-wild-reactnative
-wilddog 与 reactnative 结合的例子
+## Wilddog + React Native 开发示例
+根据官网 ListItem 示例改造而来的简单 Demo, 我们使用 Wilddog API 让它具备了实时同步数据的能力。
+
+### 开发环境
+- React Native 0.2.0 以上版本
+- Wilddog JS SDK 0.6.5 +
+
+
+### 如何运行这个 Demo
+
+
++ ```react-native init <YOUR_DIR_NAME>```
+
++ ```cd <YOUR_DIR_NAME>```
+
++ ```npm install --save wilddog``` 
+
++ 拷贝 styles.js、index.ios.js 和 components 目录中的所有内容到 ``<YOUR_DIR_NAME>``
+
++ 将 index.ios.js 的最后一行修改为 `AppRegistry.registerComponent('YOUR_DIR_NAME', () => WilddogList);`
+
++ ```react-native run-ios```
+
+### 在已有的 React Native 项目中引入 Wilddog
+
+```
+npm install --save wilddog
+```
+
+### 备注
+
+我们这里只实现了 iOS 版本的示例,所以相关代码都在 `` index.ios.js `` 中, Android 版本上使用 Wilddog 的方式与 iOS 版本并无不同之处
+
---
+++ index.ios.js
@@ -0,0 +1,113 @@
+/**
+ * 我们在这个 Demo 中尽可能的使用了 ES6 的语法, 如果你对 ES6 尚感到陌生, 推荐阅读阮一峰同学的新书——《ECMAScript 6 入门》
+ */
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    ListView,
+    StyleSheet,
+    AlertIOS,
+    Text,
+    View
+} from 'react-native';
+
+import {styles} from './styles';
+import {ActionButton} from './components/ActionButton'
+import {ListItem} from './components/ListItem'
+import {StatusBar} from './components/StatusBar'
+
+// 引入 Wilddog 依赖
+const Wilddog = require('wilddog');
+const WilddogUrl = 'https://scm.wilddogio.com/';
+
+class WilddogList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            dataSource: new ListView.DataSource({
+                rowHasChanged: (row1, row2) => row1 !== row2
+            })
+        };
+        this.itemsRef = WilddogList.getRef().child('items');
+    }
+
+    static getRef() {
+        return new Wilddog(WilddogUrl);
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <StatusBar title="Wilddog List"/>
+                <ListView
+                    dataSource={this.state.dataSource}
+                    renderRow={this._renderItem.bind(this)}
+                    style={styles.listview}/>
+                <ActionButton title="新的条目" onPress={this._addItem.bind(this)}/>
+            </View>
+        );
+    }
+
+    componentDidMount() {
+        this.listenForItems(this.itemsRef);
+    }
+
+    // 这里就是让这个 ListItem 活起来的关键所有在了
+    listenForItems(itemsRef) {
+        itemsRef.on('value', (snap) => {
+
+            // get children as an array
+            var items = [];
+            snap.forEach((child) => {
+                items.push({
+                    title: child.val().title,
+                    _key: child.key()
+                });
+            });
+
+            this.setState({
+                dataSource: this.state.dataSource.cloneWithRows(items)
+            });
+
+        });
+    }
+
+    // 新建条目的时候只需要向 wilddog 写入数据即可, 因为我们在前面调用了 itemsRef.on('value'), wilddog 数据库中的改动会被自动
+    // 同步到本地进行处理
+    _addItem() {
+        AlertIOS.prompt(
+            '新增条目',
+            '请输入要添加的条目名称',
+            [
+                {
+                    text: '确认',
+                    onPress: (text) => {
+                        this.itemsRef.push({title: text})
+                    }
+                }
+            ],
+            'plain-text'
+        );
+    }
+
+    _renderItem(item) {
+        const onPress = () => {
+            AlertIOS.prompt(
+                '删除条目',
+                '确认删除 ' + item.title + ' 吗?',
+                [
+                    {text: '删除', onPress: (text) => this.itemsRef.child(item._key).remove()},
+                    {text: '取消', onPress: (text) => console.log('已取消')}
+                ],
+                'default'
+            );
+        };
+        return (
+            <ListItem item={item} onPress={onPress}/>
+        );
+    }
+
+}
+
+AppRegistry.registerComponent('WilddogList', () => WilddogList);
